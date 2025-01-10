0bb535f9f8589c5bea231d92436448ff8a07511d
unknown <alex.chinchi02@gmail.com>
2025-01-10 09:05:05 +0100

    cambios

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <?php
-    session_start();
 
-    require_once('class.db.php');
+function inicio(){ // Funcion de inicio
+    if (isset($_POST["fIni"])) { // Si se ha pulsado el boton de enviar
+        require_once('modelo.php'); // Incluimos el modelo
+        require_once('class.db.php');
+        $db = new db(); // Creamos un objeto de la clase db
 
-    function inicio(){ // Funcion de inicio
-        if (isset($_POST["fIni"])) { // Si se ha pulsado el boton de enviar
-            require_once('modelo.php'); // Incluimos el modelo
-            $db = new db(); // Creamos un objeto de la clase db
+        if(!isset($_POST["rec"]) && isset($_COOKIE["usuario"])){ // Si no se ha pulsado el checkbox de recordar y la cookie de usuario existe
+            unset_cookie("usuario"); // Eliminamos la cookie
+        } 
 
-            if(!isset($_POST["rec"]) && isset($_COOKIE["usuario"])){ // Si no se ha pulsado el checkbox de recordar y la cookie de usuario existe
-                unset_cookie("usuario"); // Eliminamos la cookie
-            } 
-
-            if($db->compCrede($_POST["nom"], $_POST["psw"])){ // Si el usuario y la contraseña son correctos
-                if(isset($_POST["rec"])){ // Si se ha pulsado el checkbox de recordar
-                    _setcookie("usuario", $_POST["nom"]); // Creamos la cookie
-                }
-                $_SESSION["usu"] = $_POST["nom"]; // Creamos la sesion
-                $nUsu = $_POST["nom"]; // Creamos la variable de sesion
-                require_once('bienvenida.php'); // Incluimos el archivo de bienvenida
-            }else{
-                require_once('login.php'); // Incluimos el archivo de login
+        if($db->compCrede($_POST["nom"], $_POST["psw"])){ // Si el usuario y la contraseña son correctos
+            if(isset($_POST["rec"])){ // Si se ha pulsado el checkbox de recordar
+                _setcookie("usuario", $_POST["nom"]); // Creamos la cookie
             }
+            set_session("usu", $_POST["nom"]); // Creamos la sesion
+            $nUsu = $_POST["nom"]; // Creamos la variable de sesion
+            require_once('bienvenida.php'); // Incluimos el archivo de bienvenida
+        }else{
+            require_once('login.php'); // Incluimos el archivo de login
         }
     }
+}
 
     function cerrar(){ // Funcion de cerrar
-        session_unset(); // Eliminamos la sesion
-        session_destroy(); // Destruimos la sesion
+        require_once('modelo.php'); // Incluimos el modelo
+
+        unset_session("usu"); // Eliminamos la sesion
 
         header("Location: index.php"); // Redirigimos al index
     }
