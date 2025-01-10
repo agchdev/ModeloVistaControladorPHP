e5827c1f779e7ea627477e86884a63ee05a85372
unknown <alex.chinchi02@gmail.com>
2025-01-10 09:03:49 +0100

    cambios

--- index.php
+++ index.php
@@ -1,46 +1,47 @@
 <?php
     session_start();
 
-    require_once('modelo.php');
+    require_once('class.db.php');
 
-    function inicio(){
-        if (isset($_POST["fIni"])) {
-            $db = new db();
+    function inicio(){ // Funcion de inicio
+        if (isset($_POST["fIni"])) { // Si se ha pulsado el boton de enviar
+            require_once('modelo.php'); // Incluimos el modelo
+            $db = new db(); // Creamos un objeto de la clase db
 
-            if(!isset($_POST["rec"]) && isset($_COOKIE["usuario"])){
-                setcookie("usuario", "", time()-30);
+            if(!isset($_POST["rec"]) && isset($_COOKIE["usuario"])){ // Si no se ha pulsado el checkbox de recordar y la cookie de usuario existe
+                unset_cookie("usuario"); // Eliminamos la cookie
             } 
 
-            if($db->compCrede($_POST["nom"], $_POST["psw"])){
-                if(isset($_POST["rec"])){
-                    setcookie("usuario", $_POST["nom"], time()+(86400*30));
+            if($db->compCrede($_POST["nom"], $_POST["psw"])){ // Si el usuario y la contraseña son correctos
+                if(isset($_POST["rec"])){ // Si se ha pulsado el checkbox de recordar
+                    _setcookie("usuario", $_POST["nom"]); // Creamos la cookie
                 }
-                $_SESSION["usu"] = $_POST["nom"];
-                $nUsu = $_POST["nom"];
-                require_once('bienvenida.php');
+                $_SESSION["usu"] = $_POST["nom"]; // Creamos la sesion
+                $nUsu = $_POST["nom"]; // Creamos la variable de sesion
+                require_once('bienvenida.php'); // Incluimos el archivo de bienvenida
             }else{
-                require_once('login.php');
+                require_once('login.php'); // Incluimos el archivo de login
             }
         }
     }
 
-    function cerrar(){
-        session_unset();
-        session_destroy();
+    function cerrar(){ // Funcion de cerrar
+        session_unset(); // Eliminamos la sesion
+        session_destroy(); // Destruimos la sesion
 
-        header("Location: index.php");
+        header("Location: index.php"); // Redirigimos al index
     }
 
-    if(isset($_REQUEST["action"])){
-        $action = $_REQUEST["action"];
+    if(isset($_REQUEST["action"])){ // Si se ha pulsado un boton
+        $action = $_REQUEST["action"]; // Creamos la variable de accion
 
-        $action();
+        $action(); // Ejecutamos la accion
     }else{
-        if (isset($_SESSION['usu'])) {
-            $nUsu = $_SESSION['usu'];
-            require_once('bienvenida.php');
+        if (isset($_SESSION['usu'])) { // Si la sesion de usuario existe
+            $nUsu = $_SESSION['usu']; // Creamos la variable de sesion
+            require_once('bienvenida.php'); // Incluimos el archivo de bienvenida
         }else{
-            header("Location:login.php");
+            header("Location:login.php"); // Redirigimos al login
         }
     }
 ?>
--- modelo.php
+++ modelo.php
@@ -1,26 +1,30 @@
 <?php
-    class db{
-        private $conn;
-        
-        public function __construct(){
-            $this->conn = new mysqli("Localhost", "root", "", "cookies");
+    function _setcookie(String $nom, $val) { // Esta funcion establece una cookie
+        setcookie($nom, $val, time()+(86400*30)); // Esta funcion establece una cookie con un tiempo de vida de 30 dias
+    }
+
+    function unset_cookie(String $nom) { // Esta funcion elimina una cookie
+        $comp = false; // Variable para comprobar si la cookie existe
+        if(isset($_COOKIE[$nom])) { // Si la cookie existe
+            setcookie($nom, "", time()-30); // Eliminamos la cookie
+        }else{
+            $comp = true; // La cookie no existe
         }
+        return $comp; // Devolvemos el valor de la variable
+    }
 
-        public function compCrede(String $nom, String $psw){
-            $sentencia = "SELECT COUNT(*) FROM usuarios WHERE usuario = ? AND password = ?";
-            $consulta = $this->conn->prepare($sentencia);
-            $consulta->bind_param("ss", $nom, $psw);
-            $consulta->bind_result($count);
+    function set_session(String $nom, $val) { // Esta funcion establece una sesion
+        session_start(); // Esta funcion inicia una sesion
+        $_SESSION[$nom] = $val; // Esta funcion establece una sesion
+    }
 
-            $consulta->execute();
-            $consulta->fetch();
+    function get_session(String $nom){ // Esta funcion obtiene una sesion
+        session_start(); // Esta funcion inicia una sesion
+        return $_SESSION[$nom]; // Esta funcion obtiene una sesion
+    }
 
-            $existe = false;
-
-            if($count == 1) $existe = true;
-
-            $consulta->close();
-            return $existe;
-        }
+    function unset_session(String $nom) { // Esta funcion elimina una sesion
+        session_unset(); // Esta funcion elimina una sesion
+        session_destroy(); // Esta funcion destruye una sesion
     }
 ?>
